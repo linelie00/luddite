83112f4801a5e19fffe98d6b119b24277e964d7b
linelie00 <dingpawn@gmail.com>
2023-12-24 18:36:05 +0900

    api 호출 변경

--- src/useSearch.js
+++ src/useSearch.js
@@ -4,30 +4,59 @@ import axios from 'axios';
 
 const useSearch = () => {
   const [searchValue, setSearchValue] = useState('');
-  const [searchResult, setSearchResult] = useState(null);
+  const [searchResult, setSearchResult] = useState({ pos1: null, pos27: null });
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchData = useCallback(async () => {
+  const fetchData = useCallback(async (pos) => {
     try {
       setLoading(true);
       setError(null);
 
       const apiKey = "C956A2407AF0A3C67401DA0B27201261";
-      const response = await axios.get(`http://localhost:3000/api/search?certkey_no=6136&key=${apiKey}&advanced=y&type1=word&pos=1&method=start&target_type=search&req_type=json&part=word&q=${searchValue}&sort=popular&start=1&num=100&letter_s=2`);
 
-      setSearchResult(response.data);
-      console.log("호출에 성공했습니다.");
+      const parameters = {
+        certkey_no: 6136,
+        key: apiKey,
+        advanced: 'y',
+        type1: 'word',
+        pos: pos,
+        method: 'start',
+        target_type: 'search',
+        req_type: 'json',
+        part: 'word',
+        q: searchValue,
+        sort: 'popular',
+        start: 1,
+        num: 100,
+        letter_s: 2
+      };
+
+      const queryString = Object.keys(parameters)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(parameters[key])}`)
+        .join('&');
+
+      const url = `http://localhost:3000/api/search?${queryString}`;
+
+      const response = await axios.get(url);
+
+      setSearchResult(prevResult => ({
+        ...prevResult,
+        [pos === 1 ? 'pos1' : 'pos27']: response.data,
+      }));
+      console.log(`호출에 성공했습니다. (pos: ${pos})`);
     } catch (error) {
-      console.error('데이터를 불러오는 중 에러 발생:', error);
-      setSearchResult(null);
-      setError('데이터를 불러오는 중 에러 발생');
+      console.error(`데이터를 불러오는 중 에러 발생 (pos: ${pos}):`, error);
+      setSearchResult(prevResult => ({
+        ...prevResult,
+        [pos === 1 ? 'pos1' : 'pos27']: null,
+      }));
+      setError(`데이터를 불러오는 중 에러 발생 (pos: ${pos})`);
     } finally {
       setLoading(false);
     }
   }, [searchValue]);
 
-  //한글 음절만을 가능하게 함
   const isHangul = (text) => {
     const hangulRegex = /[가-힣]/;
     return hangulRegex.test(text);
@@ -42,13 +71,14 @@ const useSearch = () => {
     const handleFetchData = async () => {
       if (searchValue.trim() !== '') {
         if (isHangul(searchValue)) {
-          await fetchData();
+          await fetchData(1); // pos 값이 1일 때 호출
+          await fetchData(27); // pos 값이 27일 때 호출
         } else {
-          setSearchResult(null);
+          setSearchResult({ pos1: null, pos27: null });
           setError('잘못된 값입니다.'); // 자음과 모음뿐이거나 한글이 아닌 경우 에러
         }
       } else {
-        setSearchResult(null);
+        setSearchResult({ pos1: null, pos27: null });
         setError(null);
       }
     };
@@ -71,6 +101,3 @@ const useSearch = () => {
 };
 
 export default useSearch;
-
-
-
