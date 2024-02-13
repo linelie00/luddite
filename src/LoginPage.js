1575ccfcb5749f8817a9709cefb994e7c46ea187
linelie00 <dingpawn@gmail.com>
2024-02-13 17:04:12 +0900

    로그인 성공

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // react-router-dom에서 Link import
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import {
     HeaderIcon,
 } from './StyledComponents';
@@ -13,28 +14,51 @@ import {
     UserLink,
 } from './UsersComponents';
 
-const                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   LoginForm = () => {
-  return (
-    <UserContainer>
-          <Link to="/">
-            <HeaderIcon />
-          </Link>
-      <UserFormContainer>
-        <form>
-          <FormGroup>
-            <Label htmlFor="username">아이디:</Label>
-            <Input type="text" id="username" name="username" placeholder="아이디를 입력하세요" />
-          </FormGroup>
-          <FormGroup>
-            <Label htmlFor="password">비밀번호:</Label>
-            <Input type="password" id="password" name="password" placeholder="비밀번호를 입력하세요" />
-          </FormGroup>
-          <SubmitButton type="submit">로그인</SubmitButton>
-          <UserLink to="/signup">회원가입</UserLink>
-        </form>
-      </UserFormContainer>
-    </UserContainer>
-  );
-};
-
-export default LoginForm;
+const LoginForm = () => {
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+      const id = formData.get('id');
+      const pw = formData.get('pw');
+      
+      try {
+        const response = await axios.post('http://localhost:8282/use/login', {
+          id,
+          pw
+        });
+        if (response.status === 200) {
+            console.log('로그인 성공');
+          // 로그인 성공 시 처리
+        } else {
+            console.log('로그인 실패');
+          // 로그인 실패 시 처리
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+  
+    return (
+      <UserContainer>
+        <Link to="/">
+          <HeaderIcon />
+        </Link>
+        <UserFormContainer>
+          <form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label htmlFor="username">아이디:</Label>
+              <Input type="text" id="id" name="id" placeholder="아이디를 입력하세요" />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="password">비밀번호:</Label>
+              <Input type="password" id="pw" name="pw" placeholder="비밀번호를 입력하세요" />
+            </FormGroup>
+            <SubmitButton type="submit">로그인</SubmitButton>
+            <UserLink to="/signup">회원가입</UserLink>
+          </form>
+        </UserFormContainer>
+      </UserContainer>
+    );
+  };
+  
+  export default LoginForm;
