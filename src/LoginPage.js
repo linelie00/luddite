c09acf2878344b7f15ee32470d5a05a8a40b6969
linelie00 <dingpawn@gmail.com>
2024-02-13 14:14:46 +0900

    페이지화, 페이지 이동 버튼 개발

---
+++ src/LoginPage.js
@@ -0,0 +1,14 @@
+// LoginPage.js
+
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <h2>로그인 페이지</h2>
+      {/* 로그인 폼 등을 추가할 수 있음 */}
+    </div>
+  );
+}
+
+export default LoginPage;
